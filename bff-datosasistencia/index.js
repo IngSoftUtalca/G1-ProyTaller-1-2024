a209d1fad55a8d16b499042531139879670d0229
= <=>
2024-04-16 18:27:29 -0400

    dockerfile + docker-compose
    implementacion de archivos de DockerFile basico  para que se pueda levantar con un docker-compose
    
    docker-compose up --build -d
    docker-compose down

--- bff-datosasistencia/index.js
+++ bff-datosasistencia/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3002;
 app.get('/', (req, res) => {
-  res.json({ message: 'Micro servicio para asistencia docente' });
+  res.json({ message: 'Micro servicio para datos de asistencia' });
 });
 app.listen(PORT, () => {
   console.log('Servidor corriendo en http://localhost:' + PORT);
