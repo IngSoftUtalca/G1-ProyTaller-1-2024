3773b96900e73934bc954d2643d37c92c100d22a
Nicolas Alejandro Lopez Paredes <81203151+Nicolais174@users.noreply.github.com>
2024-04-18 16:11:30 -0400

    Create AddDocentes.sql

---
+++ DB Query/AddDocentes.sql
@@ -0,0 +1,7 @@
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 1, 'Rodrigo Paredes', '33061234-1');
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 2, 'Luis Silvestre', '33061234-2');
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 3, 'Renzo Angeles', '33061234-3');
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 4, 'Ricardo Perez', '33061234-4');
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 5, 'Rodrigo Pavez', '33061234-5');
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 6, 'Daniel Moreno', '33061234-6');
+INSERT INTO Docente (Campus, Facultad, Horario, Nombre, RUT) VALUES ('Curico', 'Ingenieria', 7, 'Ruth Garrido', '33061234-7');
