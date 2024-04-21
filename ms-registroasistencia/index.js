c258042b4303f076cae9a45677cc4f762a338f9a
= <=>
2024-04-20 20:56:23 -0400

    base para gestion de cierre automatico de las clases
    se implemento la estructura sin base de datos del cierre de las clases iniciadas cuando el profe no lo haga

--- ms-registroasistencia/index.js
+++ ms-registroasistencia/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3009;
+
+
+
+
 app.get('/', (req, res) => {
   res.json({ message: 'Micro servicio para registro asistencia' });
 });
@@ -11,9 +15,57 @@ app.listen(PORT, () => {
 app.use(express.json());
 
 app.post('/', (req, res) => {
-  console.log(req.body);
+  console.log(req);
+  console.log(req.body.Dia);
   console.log(req.url);
   console.log(req.method);
   res.json(req.body);
 
 });
+
+
+
+
+
+
+
+// este se encargara de la gestion del cierre de la clase en el caso de que el profe no lo cierre. esto se hara de manera asincrona 
+async function monitorearHorario(){
+    try{
+        var hora = new Date();
+        //console.log(hora.getHours()+":"+hora.getMinutes());
+        // consultar bloque en que esta y se descontaria la hora en que esta 
+
+
+        var horaespera = 360*1000 ; // se definira la hora de cierre 
+
+
+        while(conectado){ // esta parte se 
+            const respuesta = await procesarHora(horaespera); 
+            console.log(respuesta);
+
+
+            //hora = new Date();
+           // console.log(hora.getHours()+":"+hora.getMinutes());
+        }
+    }catch(error){
+    }
+};
+conectado = true;
+function procesarHora(segundos){
+    return new Promise((resolve,reject) => {
+        console.log(`iniciado nuevo ciclo de 1 hora`);
+        setTimeout(()=>{
+            console.log(`solicitando entrada a la BD`);
+            if(conectado == true){
+                resolve('cerrando los horarios');
+            }else{
+                reject('no se puede conectar a la BD');
+            }
+        },segundos)
+    });
+}
+
+//console.log(process.env.TZ);
+process.env.TZ = 'America/Santiago'; // se define la zona horaria en chile
+monitorearHorario();
