c1c0fe50b386bb849d8ffdefe76c2572d80f7311
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-04-22 14:32:42 -0400

    Agregar periodos listo

--- ms-registrohorarios/index.js
+++ ms-registrohorarios/index.js
@@ -1,30 +1,19 @@
-const { Console } = require('console');
 const express = require('express');
-const { get } = require('http');
 const mysql = require('mysql');
 const app = express();
 const ProgressBar = require('progress');
-app.use(express.json({ limit: '50mb' }));
-app.use(express.json());
 
-const bloques = [
-  {id: 1, inicio: '08:30:00', fin: '09:30:00'},
-  {id: 2, inicio: '09:40:00', fin: '10:40:00'},
-  {id: 3, inicio: '10:50:00', fin: '11:50:00'},
-  {id: 4, inicio: '12:00:00', fin: '13:00:00'},
-  {id: 5, inicio: '13:10:00', fin: '14:10:00'},
-  {id: 6, inicio: '14:20:00', fin: '15:20:00'},
-  {id: 7, inicio: '15:30:00', fin: '16:30:00'},
-  {id: 8, inicio: '16:40:00', fin: '17:40:00'},
-  {id: 9, inicio: '17:50:00', fin: '18:50:00'},
-  {id: 10, inicio: '19:00:00', fin: '20:00:00'},
-  {id: 11, inicio: '20:10:00', fin: '21:10:00'},
-  {id: 12, inicio: '21:20:00', fin: '22:20:00'},
-];
+const funciones = require('./shared/funciones.js');
+const { get } = require('http');
+const runQuery = funciones.runQuery;
+const getBloqueId = funciones.getBloqueId;
+const getInstancia = funciones.getInstancia;
+
+app.use(express.json({ limit: '50mb' }));
 
 const dbData = require('../ENPOINTS.json').DB;
 
-app.post('/nuevo', async (req, res) => {
+app.post('/', async (req, res) => {
   const connection = mysql.createConnection(dbData);
 
   connection.connect(error => {
@@ -36,7 +25,6 @@ app.post('/nuevo', async (req, res) => {
 
   const XLSX = require('xlsx');
   const fs = require('fs');
-  const FileSaver = require('file-saver');
 
   // Get data from request body
   const fechaInicio = req.body.fechaInicio;
@@ -46,6 +34,23 @@ app.post('/nuevo', async (req, res) => {
 
   const buffer = Buffer.from(horario_periodo, 'base64');
 
+
+  // Get all the dates between fechaInicio and fechaTermino
+  const datesInRange = [];
+  const currentDate = new Date(fechaInicioDate);
+  while (currentDate <= fechaTerminoDate) {
+    datesInRange.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  // Filter out the weekends (Saturday and Sunday)
+  const filteredDates = datesInRange.filter(date => date.getDay() !== 0 && date.getDay() !== 6);
+
+  // Filter out the dates that are already in feriados
+  const newFeriados = filteredDates.filter(date => !feriados.includes(date.toISOString().split('T')[0]));
+
+  // Add the new feriados to the existing feriados array
+  feriados.push(...newFeriados);
   // Guardar el archivo .xlsx
   fs.writeFileSync('archivo.xlsx', buffer);
 
@@ -58,12 +63,7 @@ app.post('/nuevo', async (req, res) => {
   // Convertir la hoja a JSON
   const worksheet = workbook.Sheets[sheetName];
   const jsonData = XLSX.utils.sheet_to_json(worksheet);
-/*  
-  const nombresramos = jsonData.map(obj => ({
-    NOMBRE: obj.NOMBRE,
-    LIGA: obj.LIGA
-  }));
-  */
+  
   const nombresSecciones = [...new Set(jsonData.map(obj => {
     const seccion = obj.LIGA.split("-")[1];
     const nombre = obj.NOMBRE.replace('(CURICO)', '').trim();
@@ -71,26 +71,7 @@ app.post('/nuevo', async (req, res) => {
   }))];
 
   // Extraer las salas
-  const instancia = jsonData.map(obj => {
-    let sala = obj.SALA;
-    let edificio = obj.EDIFICIO;
-    const inicioDecimal = obj["HORA INICIO"];
-    const terminoDecimal = obj["HORA FIN"];
-    const seccion = obj.LIGA.split("-")[1];
-    const nombre = obj.NOMBRE.replace('(CURICO)', '').trim();
-
-    const inicio = inicioDecimal ? `${Math.floor(inicioDecimal * 24)}:${Math.round((inicioDecimal * 24 - Math.floor(inicioDecimal * 24)) * 60).toString().padStart(2, '0')}` : '';
-    const termino = terminoDecimal ? `${Math.floor(terminoDecimal * 24)}:${Math.round((terminoDecimal * 24 - Math.floor(terminoDecimal * 24)) * 60).toString().padStart(2, '0')}` : '';
-    if (inicio === 'NaN' || termino === 'NaN') {
-      return null;
-    }
-    const dia = obj.DIA;
-    if (!sala && !edificio) {
-      return {SALA: 'SIN SALA'};
-    }
-    return {SALA: `${edificio}-${sala}`, INICIO: `${inicio}`, TERMINO: `${termino}`,DIA: `${dia}` , NOMBRE: `${nombre} Seccion ${seccion}`};
-    
-  }).filter(inst => inst !== null);
+  const instancia = getInstancia(jsonData);
 
   // Split fechaInicio and fechaTermino into components
   const [yearInicio, monthInicio, dayInicio] = fechaInicio.split('-').map(Number);
@@ -112,19 +93,28 @@ app.post('/nuevo', async (req, res) => {
   // Construct the constant
   const semestre = 'Semestre.' + semesterInicio + '-' + yearInicio;
 
+  const queryLimpieza = `DELETE FROM \`Periodo\` WHERE ID = ?;`;
+  try{
+    await runQuery(connection, queryLimpieza, [semestre]);
+  }catch(error){
+    console.log("no se pudo borrar semestre: ", error.message);
+  }
+
   const query1 = `INSERT INTO \`Periodo\` (ID, fechaInicio, fechaTermino) VALUES (?, ?, ?);`;
   try {
-    await runQuery(connection, query1, [semestre, fechaInicioDate, fechaTerminoDate]);
+    await funciones.runQuery(connection, query1, [semestre, fechaInicioDate, fechaTerminoDate]);
   } catch (error) {
-    return res.status(500).json({ error: 'Error ejecutando la query dentro de Periodo' });
+    return res.status(500).json({ error: 'Error ejecutando la query dentro de Periodo: '+error.message });
   }
 
   console.log("Periodo creado");
 
   const query2 = `INSERT INTO \`Dia_Libre\` (Dia, ID_periodo ) VALUES (?, ?);`;
+  const bar = new ProgressBar('Agregando feriados [:bar] :percent', { total: feriados.length });
   for (let feriado of feriados) {
     try {
       await runQuery(connection, query2, [feriado, semestre]);
+      bar.tick();
     } catch (error) {
       return res.status(500).json({ error: 'Error ejecutando la query dentro de Dia_Libre' });
     }
@@ -172,31 +162,3 @@ app.post('/nuevo', async (req, res) => {
 app.listen(3010, () => {
   console.log('Server running on http://localhost:3010');
 });
-
-function getBloqueId(hora) {
-  if (!hora) {
-    return null;
-  }
-  const [horaH, horaM] = hora.split(':').map(Number);
-  for (const bloque of bloques) {
-    const [inicioH, inicioM] = bloque.inicio.split(':').map(Number);
-    const [finH, finM] = bloque.fin.split(':').map(Number);
-    if ((horaH > inicioH || (horaH === inicioH && horaM >= inicioM)) && (horaH < finH || (horaH === finH && horaM <= finM))) {
-      return bloque.id;
-    }
-  }
-  return null;
-}
-
-function runQuery(connection, query, params) {
-  return new Promise((resolve, reject) => {
-    connection.query(query, params, (error, results) => {
-      if (error) {
-        console.error('Error ejecutando la query: ', error);
-        reject(error);
-      } else {
-        resolve(results);
-      }
-    });
-  });
-}
