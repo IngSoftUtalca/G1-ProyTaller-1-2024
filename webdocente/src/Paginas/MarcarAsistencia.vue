4b7ae6370a00f3f661c698aea610beaa5372f2fb
rafa <rafajeremias@live.com>
2024-04-21 23:46:13 -0400

    Migrando

--- webdocente/src/Paginas/MarcarAsistencia.vue
+++ webdocente/src/Paginas/MarcarAsistencia.vue
@@ -1,43 +1,113 @@
-<template>
-    <div class="error-page">
-        <h1>Sala {{ idSala }}</h1>
-        <p>esto es para probar la sala dinamica</p>
+<template> 
+  <center>
+  <div id="app" class="fondo" >
+    <!-- Imagen en la parte superior -->
+    <div class="row justify-content-center">
+      <img src="https://cdn.utalca.cl/img_utal/ins/utal.png" alt="Logo" class="img-fluid" style="max-width: 200px; max-height: 200px;">
     </div>
+    <!-- Contenido principal -->
+    <div class="row justify-content-center">
+          <div class="card-body">
+            <h3 class="ramos"> {{ramo}} B1 8:30-9:30</h3>
+          </div>
+    </div>
+    <!-- Botón de asistencia -->
+    <div>
+      <button type="button" class="btn btn-outline-warning">Confirmar</button>
+    </div>
+  </div>
+</center>
 </template>
 
-<style scoped>
-.error-page {
+<style>
+  .fondo {
+    width: 393px;
+    height: 852px;
+    flex-grow: 0;
+    padding: 54px 46px 168px 47px;
+    background-color: #f4f4f4;
+  }
+   .logo{
+    width: 213px;
+    height: 48px;
+    margin: 56px 44px 90px 43px;
+    padding: 0 0.4px 0 0;
+  }
+  
+  .ramos{
+    width: 254px;
+    height: 30px;
+    flex-grow: 0;
+    margin: 100px 0 0;
+    font-size: 32px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
     text-align: center;
-    margin-top: 100px;
-}
-
-h1 {
-    font-size: 48px;
-    color: #ff0000;
-}
-
-p {
-    font-size: 24px;
-    color: #333333;
-}
-</style>
+    color: #fff;
+  }
+  
+  
+  .card-body {
+  
+    width: 300px;
+    height: 300px;
+    flex-grow: 0;
+    margin: 5px 0;
+    padding: 113px 23px;
+    border-radius: 16px;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+    background-color: #60cdcd;
+  
+  
+  }
+  
+  .btn-outline-warning {
+    width: 185px;
+    height: 46px;
+    margin: 90px 58px 0 57px;
+    padding: 11px 32px 12px 33px;
+    border-radius: 16px;
+    color: orange;
+    
+  }
+  
+  .img-fluid {
+    width: 213px;
+    height: 48px;
+    margin: 56px 44px 90px 43px;
+    padding: 0 0.4px 0 0;
+  }
+  </style>
 
 <script>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 export default {
+  
   setup() {
     const route = useRoute()
     const idSala = ref(route.params.idSala)
 
     onMounted(() => {
+      console.log('Fetching data for sala', idSala.value)
+      idSalas.value = "Taller de Software"
+
       // Fetch data for this sala here
     })
 
     return {
       idSala
     }
-  }
+  },
+  methods: {
+      marcarAsistencia() {
+        // Aquí puedes agregar la lógica para marcar la asistencia
+        console.log('Asistencia marcada');
+      }
+    }
 }
 </script>
