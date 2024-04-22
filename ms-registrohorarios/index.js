9c7b984afa56c361ae2572b3baefbd4f48309f5d
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-04-21 23:07:31 -0400

    Trabajando en agregar instancias

--- ms-registrohorarios/index.js
+++ ms-registrohorarios/index.js
@@ -1,8 +1,25 @@
+const { Console } = require('console');
 const express = require('express');
+const { get } = require('http');
 const mysql = require('mysql');
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
+
+const bloques = [
+  {id: 1, inicio: '08:30:00', fin: '09:30:00'},
+  {id: 2, inicio: '09:40:00', fin: '10:40:00'},
+  {id: 3, inicio: '10:50:00', fin: '11:50:00'},
+  {id: 4, inicio: '12:00:00', fin: '13:00:00'},
+  {id: 5, inicio: '13:10:00', fin: '14:10:00'},
+  {id: 6, inicio: '14:20:00', fin: '15:20:00'},
+  {id: 7, inicio: '15:30:00', fin: '16:30:00'},
+  {id: 8, inicio: '16:40:00', fin: '17:40:00'},
+  {id: 9, inicio: '17:50:00', fin: '18:50:00'},
+  {id: 10, inicio: '19:00:00', fin: '20:00:00'},
+  {id: 11, inicio: '20:10:00', fin: '21:10:00'},
+  {id: 12, inicio: '21:20:00', fin: '22:20:00'},
+];
 
 const dbData = require('../ENPOINTS.json').DB;
 
@@ -52,16 +69,27 @@ app.post('/nuevo', (req, res) => {
     }))];
 
     // Extraer las salas
-    const salas = [...new Set(jsonData.map(obj => {
+    const instancia = jsonData.map(obj => {
       let sala = obj.SALA;
       let edificio = obj.EDIFICIO;
-      if (!sala && !edificio) {
-        return 'SIN SALA';
+      const inicioDecimal = obj["HORA INICIO"];
+      const terminoDecimal = obj["HORA FIN"];
+      const seccion = obj.LIGA.split("-")[1];
+      const nombre = obj.NOMBRE.replace('(CURICO)', '').trim();
+
+      const inicio = inicioDecimal ? `${Math.floor(inicioDecimal * 24)}:${Math.round((inicioDecimal * 24 - Math.floor(inicioDecimal * 24)) * 60).toString().padStart(2, '0')}` : '';
+      const termino = terminoDecimal ? `${Math.floor(terminoDecimal * 24)}:${Math.round((terminoDecimal * 24 - Math.floor(terminoDecimal * 24)) * 60).toString().padStart(2, '0')}` : '';
+      if (inicio === 'NaN' || termino === 'NaN') {
+        return null;
       }
-      return `${edificio}-${sala}`;
-    }))];
-    
-    return res.json({ message: 'Trabajando en parseo' });
+      const dia = obj.DIA;
+      if (!sala && !edificio) {
+        return {SALA: 'SIN SALA'};
+      }
+      return {SALA: `${edificio}-${sala}`, INICIO: `${inicio}`, TERMINO: `${termino}`,DIA: `${dia}` , NOMBRE: `${nombre} Seccion ${seccion}`};
+      
+    }).filter(inst => inst !== null);
+
     // Split fechaInicio and fechaTermino into components
     const [yearInicio, monthInicio, dayInicio] = fechaInicio.split('-').map(Number);
     const [yearTermino, monthTermino, dayTermino] = fechaTermino.split('-').map(Number);
@@ -89,6 +117,8 @@ app.post('/nuevo', (req, res) => {
         return res.status(500).json({ error: 'Error ejecutando la query dentro de Periodo' });
       }
 
+      console.log("Periodo creado");
+
       const query2 = `INSERT INTO \`Dia_Libre\` (Dia, ID_periodo ) VALUES (?, ?);`;
       for (let feriado of feriados) {
         connection.query(query2, [feriado, semestre], (error, results) => {
@@ -99,6 +129,8 @@ app.post('/nuevo', (req, res) => {
         });
       }
 
+      console.log("Feriados agregados");
+
       const query3 = `INSERT INTO \`Ramo\` (Nombre, Periodo) VALUES (?, ?);`;
       for (let nombre of nombresSecciones) {
         connection.query(query3, [nombre, semestre], (error, results) => {
@@ -108,6 +140,26 @@ app.post('/nuevo', (req, res) => {
           }
         });
       }
+      
+      console.log("Ramos agregados");
+      
+      const query4 = `INSERT INTO \`Instancia\` (Sala, Bloque, Dia_Semana, Ramo) VALUES (?, ?, ?, ?);`;
+      for (let ins of instancia){
+        const inicio = getBloqueId(ins.INICIO);
+        const termino = getBloqueId(ins.TERMINO);
+        for (let i = inicio; i < termino; i++) {
+          console.log(query4, [ins.SALA, i, ins.DIA, ins.NOMBRE]);
+          connection.query(query4, [ins.SALA, i, ins.DIA, ins.NOMBRE], (error, results) => {
+            if (error) {
+              console.error('Error ejecutando la query: ', error);
+              return res.status(500).json({ error: 'Error ejecutando la query dentro de Instancia' });
+            }
+          });
+        
+        }
+      }
+
+      
       connection.end();
 
       res.json({ message: 'Operación terminada con exito' });
@@ -118,3 +170,18 @@ app.post('/nuevo', (req, res) => {
 app.listen(3010, () => {
   console.log('Server running on http://localhost:3010');
 });
+
+function getBloqueId(hora) {
+  if (!hora) {
+    return null;
+  }
+  const [horaH, horaM] = hora.split(':').map(Number);
+  for (const bloque of bloques) {
+    const [inicioH, inicioM] = bloque.inicio.split(':').map(Number);
+    const [finH, finM] = bloque.fin.split(':').map(Number);
+    if ((horaH > inicioH || (horaH === inicioH && horaM >= inicioM)) && (horaH < finH || (horaH === finH && horaM <= finM))) {
+      return bloque.id;
+    }
+  }
+  return null;
+}
