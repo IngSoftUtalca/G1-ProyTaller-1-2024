0b22c2a3e2b37ccca7c7990cbdc7b0e139745bf2
VCornejo10 <vcornejo19@alumnos.utalca.cl>
2024-04-25 15:46:18 -0400

    Arreglo visual pagina reporte
    Pequeño arreglo visual en pagina reporte, esta malo el internet del lab jaja

--- webdocente/src/Paginas/reportar_error.vue
+++ webdocente/src/Paginas/reportar_error.vue
@@ -8,11 +8,10 @@
         <div class="row justify-content-center">
           <div class="card-blanca">
             <div class="row justify-content-center">
-              <img src="../assets/utalca.svg" alt="Logo" class="logo_utalca" style="max-width: 200px; max-height: 200px;">
-              
+              <img src="../assets/utalca.svg" alt="Logo" class="logo_utalca" style="max-width: 200px; max-height: 200px;">  
             </div>
             <p class="texto">Cuentanos que pasó:</p>
-            <select class="form-control" v-model="selectedOption">
+            <select class="combobox" v-model="selectedOption" placeholder="Seleccione un tipo de error">
               <option value="error_de_red">Error de red</option>
               <option value="error_de_gps">Error de GPS</option>
               <option value="no_hay_internet">No hay internet</option>
@@ -21,7 +20,7 @@
             </select>
             <p class="texto">Detalles (Opcional):</p>
             
-            <input type="text" class="form-control" v-model="details" placeholder="Detalles">
+            <input type="text" class="detalles" v-model="details" placeholder="Detalles">
             
             <button type="button" class="btn boton_rojo" @click="enviarReporte">Enviar reporte</button>
           </div>
@@ -58,6 +57,16 @@
   .texto{
     font-size: 16px;
     text-align: left;
+  }
+  .detalles{
+    height: 25%;
+    width: 100%;
+    resize: none;
+    padding-top:100%;
+    font-size: 16px;
+  }
+  .combobox{
+    width: 100%;
   }
   </style>
   
