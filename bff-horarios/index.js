6dae543a469ea541ecbe0172d22a77fce585c393
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-05-06 13:31:18 -0400

    Falta afinar la modificación de periodos pero en general el resto está listo

--- bff-horarios/index.js
+++ bff-horarios/index.js
@@ -18,8 +18,34 @@ app.get('/periodos', async (req, res) => {
     connection.connect();
     const query = 'SELECT * FROM Periodo';
     runQuery(connection, query)
-      .then((results) => {
-        res.status(200).json(results);
+      .then(async (results) => {
+        const updatedResults = results.map(async periodo => {
+          const fechaInicio = new Date(periodo.FechaInicio);
+          const fechaTermino = new Date(periodo.FechaTermino);
+          const fechaActual = new Date();
+
+          if (fechaInicio <= fechaActual && fechaActual <= fechaTermino) {
+            periodo.Estado = 'Activo';
+          } else if (fechaTermino < fechaActual) {
+            periodo.Estado = 'Finalizado';
+          } else if (fechaActual < fechaInicio) {
+            periodo.Estado = 'Pendiente';
+          }
+
+          const updateQuery = `UPDATE Periodo SET Estado = '${periodo.Estado}' WHERE id = '${periodo.ID}'`;
+          await runQuery(connection, updateQuery);
+
+          return periodo;
+        });
+
+        await runQuery(connection, query)
+          .then((results) => {
+            res.status(200).json(results);
+          })
+          .catch((e) => {
+            console.error('Error ejecutando la query: ', e);
+            return res.status(500).json({ message: 'Error' });
+          });
       })
       .catch((e) => {
         console.error('Error ejecutando la query: ', e);
