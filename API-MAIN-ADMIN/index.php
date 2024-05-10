c810c7a6e1d7ec76f840234e32869af03324067e
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-05-10 19:21:09 -0400

    codigos configurador fala hostearlos y integrar en paginas

---
+++ API-MAIN-ADMIN/index.php
@@ -0,0 +1,17 @@
+<?php
+/* Validar que sesión no haya expirado */
+session_start();
+if (!isset($_SESSION['sso'])) {
+    session_unset();
+    session_destroy();
+    // Ubicación de archivo -> inter.php.
+    Header("Location: inter.php?userType=".$user_type);
+}else{
+    $id = $_SESSION['id'];
+    $mainpage = file_get_contents('../ENPOINTS.json');
+    $mainpage = json_decode($mainpage, true);
+    $mainpage = $mainpage['mainpage'];
+    $mainpage = $mainpage."/administrador"."/".$id;
+    header("location:".$mainpage);
+}
+?>
