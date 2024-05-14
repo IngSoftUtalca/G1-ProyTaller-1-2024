68a69407acded7a47a5e43e5931839a26c5b6abd
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-05-14 01:24:55 -0400

    Ajustes visuales en webdocente

--- webdocente/src/Paginas/clase_iniciada.vue
+++ webdocente/src/Paginas/clase_iniciada.vue
@@ -1,19 +1,19 @@
 <template> 
     <center>
-      <div id="app" class="fondo" >
+      <div id="app" class="fondo container-fluid" >
         <!-- Imagen en la parte superior -->
         <div class="row justify-content-center">
           <img src="../assets/utalca.svg" alt="Logo" class="logo_utalca" style="max-width: 200px; max-height: 200px;">
         </div>
         <!-- Contenido principal -->
-        <div class="row justify-content-center">
+        <div class="row container d-flex justify-content-center align-items-center p-0 m-0">
           <div class="card-celeste">
-            <h3 class="ramos"> Clase iniciada {{ inicio }}</h3>
+            <h3 class="w-100 ramos text-center"> Clase iniciada <br> {{ inicio }}</h3>
           </div>
         </div>
         <!-- Botón de asistencia -->
         <div>
-          <button type="button" class="btn boton_gris" :class="{ 'boton-amarillo': botonC }" @click="claseiniciada">Finalizar Clase</button>
+          <button type="button" class="btn boton_gris text-white bold" :class="{ 'boton-amarillo': botonC }" @click="claseiniciada">Finalizar Clase</button>
         </div>
       </div>
     </center>
