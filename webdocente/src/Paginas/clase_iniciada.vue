d38b2e7739dd0cac9eb22e49d772e106c88ef56b
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-05-16 15:53:49 -0400

    Mejoras visuales

--- webdocente/src/Paginas/clase_iniciada.vue
+++ webdocente/src/Paginas/clase_iniciada.vue
@@ -1,47 +1,53 @@
 <template>
-  <center>
-    <div id="app" class="fondo container-fluid">
-      <!-- Imagen en la parte superior -->
-      <div class="row justify-content-center">
-        <img
-          src="../assets/utalca.svg"
-          alt="Logo"
-          class="logo_utalca"
-          style="max-width: 200px; max-height: 200px"
-        />
-      </div>
-      <!-- Contenido principal -->
-      <div
-        class="row d-flex justify-content-center align-items-center text-center"
-        v-if="!loading"
-      >
-        <div class="card-celeste container">
-          <div
-            class="row d-flex h-50 justify-content-center align-items-end m-0 py-0"
-          >
-            <p class="ramos">clase iniciada</p>
-          </div>
-          <div
-            class="row d-flex h-50 justify-content-center align-items-start m-0 py-0"
-          >
-            <p class="ramos">{{ inicio }}</p>
-          </div>
-          <!-- <h3 class="ramos"> {{ramo}}Taller de Software B1 8:30-9:30</h3> -->
-        </div>
-      </div>
-      <!-- Botón de asistencia -->
-      <div>
-        <button
-          type="button"
-          class="btn boton_gris text-white bold"
-          :class="{ 'boton-amarillo': botonC }"
-          @click="claseiniciada"
+  <div id="app" class="fondo container-fluid">
+    <!-- Imagen en la parte superior -->
+    <div class="row justify-content-center">
+      <img
+        src="../assets/utalca.svg"
+        alt="Logo"
+        class="logo_utalca"
+        style="max-width: 200px; max-height: 200px"
+      />
+    </div>
+    <!-- Contenido principal -->
+    <div
+      class="row d-flex justify-content-center align-items-center text-center"
+      v-if="!loading"
+    >
+      <div class="card-celeste container">
+        <div
+          class="row d-flex h-50 justify-content-center align-items-end m-0 py-0"
         >
-          Finalizar Clase
-        </button>
+          <p class="ramos">Clase iniciada</p>
+        </div>
+        <div
+          class="row d-flex h-50 justify-content-center align-items-start m-0 py-0"
+        >
+          <p class="ramos">{{ inicio }}</p>
+        </div>
+        <!-- <h3 class="ramos"> {{ramo}}Taller de Software B1 8:30-9:30</h3> -->
       </div>
     </div>
-  </center>
+    <div
+      class="container h-75 d-flex align-items-center justify-content-center"
+      v-if="loading"
+    >
+      <div class="spinner-grow text-primaryC loading" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    <!-- Botón de asistencia -->
+    <div>
+      <button
+        class="btn boton_gris text-white bold"
+        type="button"
+        @click="claseiniciada"
+        v-if="!loading"
+      >
+        Finalizar Clase
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -55,9 +61,10 @@ export default {
       botonC: false,
     };
   },
-  mounted() {
+  async mounted() {
     const moment = require("moment-timezone");
     this.inicio = moment().tz("America/Santiago").format("HH:mm");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     this.loading = false;
   },
   methods: {
