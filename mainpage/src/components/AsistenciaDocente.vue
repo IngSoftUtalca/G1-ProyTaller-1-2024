4a53bdc171a2402c0d88a75ddcb32cd1773dd8dc
VCornejo10 <vcornejo19@alumnos.utalca.cl>
2024-05-23 16:15:41 -0400

    Arreglos de codigo

--- mainpage/src/components/AsistenciaDocente.vue
+++ mainpage/src/components/AsistenciaDocente.vue
@@ -1,22 +1,29 @@
 <template>
     <!-- loading-->
     <div class="container d-flex justify-content-center align-items-center h-450" v-if="loading">
+        
         <div class="spinner-grow primary-normal div-size-72" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
+    
     </div>
+    
     <div class="background">
         <div class="row container-fluid" v-if="!loading">
             <div class="row px-5 rt-50 h-55 font-20 bold primary-bg d-flex align-items-center">
+
                 <div class="col-2 text-center">
                     Bloque
                 </div>
+                
                 <div class="col-2 text-center">
                     Curso
                 </div>
+                
                 <div class="col-2 text-center">
                     Fecha
                 </div>
+                
                 <div class="col-2 text-center">
                     Estado
                 </div>
@@ -24,18 +31,18 @@
                 <div class="col-2 text-center">
                     Justificacion
                 </div>
+                
                 <div class="col-2 text-center">
-                    s
+                    <!--Columna para el botn justificar-->
                 </div>
+
             </div>
         </div>
 
         <!--
         Tuplas
         -->
-        <div class="row h-100 px-5 secondary-bg text-center bold d-flex align-items-center"
-            v-for="(asistencia, index) in asistencias" :key="index">
-
+        <div class="row h-100 px-5 secondary-bg text-center bold d-flex align-items-center" v-for="(asistencia, index) in asistencias" :key="index">
             <div class="col-2 text-center">
                 {{ getBloque(asistencia.bloque) }}
             </div>
@@ -49,6 +56,7 @@
             </div>
             
             <div class="col-2 d-flex justify-content-center align-items-center">
+
                 <div class="pill-yellow pill-size-150" v-if="asistencia.Estado == 'No asistido'">
                     {{ asistencia.Estado }}
                 </div>
@@ -56,12 +64,15 @@
                 <div class="pill-green pill-size-150" v-if="asistencia.Estado == 'asistio'">
                     {{ asistencia.Estado }}
                 </div>
+
             </div>
             
             <div class="col-2 text-center">
+
                 <div class="pill-yellow pill-size-150" v-if="asistecia.justificacion == null">
                     No justificado
                 </div>
+
                 <div class="pill-yellow pill-size-150" v-if="asistecia.justificacion != null">
                     {{ getJustificacion(asistecia.justificacion) }}
                 </div>
@@ -79,11 +90,9 @@
 </template>
 
 <script>    
-    //import moment from 'moment';
-    
+    //import moment from 'moment';    
     //import axios from 'axios';
-    //import ENDPOINTS from '../../../ENPOINTS.json';
-    
+    //import ENDPOINTS from '../../../ENPOINTS.json';   
     export default {
         name: 'AsistenciaDocente',
         data() {
@@ -100,8 +109,7 @@
         methods: {
             async getAsistencias() {
                 try {
-                    //const response = await axios.get(ENDPOINTS['bff-datosasistencia'] + '/datosasistencias');
-                    
+                    //const response = await axios.get(ENDPOINTS['bff-datosasistencia'] + '/datosasistencias');    
                     this.loading = false;
                 } catch (error) {
                     this.loading = false;
@@ -115,6 +123,5 @@
                 this.OverlayJustificacion = false;
             }
         }
-        
     }
 </script>
