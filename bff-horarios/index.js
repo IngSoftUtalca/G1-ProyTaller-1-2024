fe53c7264244e900a8871abea1d834600673d8cb
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-05-29 20:06:10 -0400

    Parte superior de la tabla y backend para concer el horario del docente

--- bff-horarios/index.js
+++ bff-horarios/index.js
@@ -13,6 +13,24 @@ app.get('/', (req, res) => {
   res.json({ message: 'Micro servicio para horarios' });
 });
 
+app.get('/asignaciones/:rut', async (req, res) => {
+  try {
+    const rut = req.params.rut;
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect();
+    const query = `SELECT * FROM Instancia WHERE Ramo = (SELECT Nombre_Ramo FROM Asignacion WHERE RUT_Docente = ?) AND Semestre = (SELECT Periodo.ID FROM Periodo WHERE Estado = 'Activo');`;
+    await runParametrizedQuery(connection, query, [rut]).then((results) => {
+      res.status(200).json(results);
+    }).catch((e) => {
+      console.error('Error ejecutando la query: ', e);
+      return res.status(500).json({ message: 'Error' });
+    });
+  } catch (e) {
+    console.error('Error conectando a la base de datos: ', e);
+    return res.status(500).json({ message: 'Error' });
+  }
+});
+
 app.get('/periodos', async (req, res) => {
   try {
     const connection = mysql.createConnection(dbConfig);
@@ -59,75 +77,75 @@ app.get('/periodos', async (req, res) => {
 });
 
 app.get('/instancia', async (req, res) => {
-    const constantes = require('./constantes.json');
-    let startTime = "";
-    let endTime = "";
-    try {
-        const connection = mysql.createConnection(dbConfig);
-        connection.connect();
-        const {sala, dia, bloque} = req.query;
-        semestre = await runQuery(connection, `SELECT * FROM Periodo WHERE Estado = 'Activo'`);
-        semestre = semestre[0].ID;
-        if (!sala || !dia || !bloque) {
-            return res.status(400).json({ message: 'Faltan parametros' });
-        }else if (dia < 1 || dia > 5) {
-            return res.status(400).json({ message: 'Dia no valido' });
-        }else if (semestre == null) {
-            return res.status(400).json({ message: 'No hay semestre activo' });
-        }
-        try {
-            let query = `SELECT Ramo FROM Instancia WHERE Sala = ? AND Dia_Semana = ? AND Bloque = ? AND Semestre = ?`;
-            let ramo = await runParametrizedQuery(connection, query, [sala, dia, bloque, semestre]);
-            ramo = ramo[0].Ramo;
-            query = `SELECT Bloque FROM Instancia WHERE Ramo = ? AND Semestre = ? AND Dia_Semana = ?;`
-            const bloques = await runParametrizedQuery(connection, query, [ramo, semestre, dia]);
-            const bloqueStart = bloques[0].Bloque;
-            const bloqueEnd = bloques[bloques.length - 1].Bloque;
-            query = `SELECT * FROM Bloque WHERE ID = ?;`
-            startTime = await runParametrizedQuery(connection, query, [bloqueStart]);
-            endTime = await runParametrizedQuery(connection, query, [bloqueEnd]);
-            const response = {
-                Ramo: ramo,
-                HoraInicio: startTime[0].Inicio,
-                HoraTermino: endTime[0].Termino,
-            }
-            return res.status(200).json(response);
-        }catch (e) {
-            const bloques = constantes.bloques;
-            for(const b of bloques) {
-                if (b.id == bloque) {
-                    startTime = b.inicio;
-                    endTime = b.fin;
-                }
-            }
-            const response = {
-                Ramo: "No hay clases en la sala",
-                HoraInicio: startTime,
-                HoraTermino: endTime,
-            }
-            return res.status(200).json(response);
-        }
-        connection.end();
-
-    } catch (e) {
-        console.log('Error conectando a la base de datos: ', e);
-        return res.status(500).json({ message: 'Error: '+e });
+  const constantes = require('./constantes.json');
+  let startTime = "";
+  let endTime = "";
+  try {
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect();
+    const { sala, dia, bloque } = req.query;
+    semestre = await runQuery(connection, `SELECT * FROM Periodo WHERE Estado = 'Activo'`);
+    semestre = semestre[0].ID;
+    if (!sala || !dia || !bloque) {
+      return res.status(400).json({ message: 'Faltan parametros' });
+    } else if (dia < 1 || dia > 5) {
+      return res.status(400).json({ message: 'Dia no valido' });
+    } else if (semestre == null) {
+      return res.status(400).json({ message: 'No hay semestre activo' });
     }
+    try {
+      let query = `SELECT Ramo FROM Instancia WHERE Sala = ? AND Dia_Semana = ? AND Bloque = ? AND Semestre = ?`;
+      let ramo = await runParametrizedQuery(connection, query, [sala, dia, bloque, semestre]);
+      ramo = ramo[0].Ramo;
+      query = `SELECT Bloque FROM Instancia WHERE Ramo = ? AND Semestre = ? AND Dia_Semana = ?;`
+      const bloques = await runParametrizedQuery(connection, query, [ramo, semestre, dia]);
+      const bloqueStart = bloques[0].Bloque;
+      const bloqueEnd = bloques[bloques.length - 1].Bloque;
+      query = `SELECT * FROM Bloque WHERE ID = ?;`
+      startTime = await runParametrizedQuery(connection, query, [bloqueStart]);
+      endTime = await runParametrizedQuery(connection, query, [bloqueEnd]);
+      const response = {
+        Ramo: ramo,
+        HoraInicio: startTime[0].Inicio,
+        HoraTermino: endTime[0].Termino,
+      }
+      return res.status(200).json(response);
+    } catch (e) {
+      const bloques = constantes.bloques;
+      for (const b of bloques) {
+        if (b.id == bloque) {
+          startTime = b.inicio;
+          endTime = b.fin;
+        }
+      }
+      const response = {
+        Ramo: "No hay clases en la sala",
+        HoraInicio: startTime,
+        HoraTermino: endTime,
+      }
+      return res.status(200).json(response);
+    }
+    connection.end();
+
+  } catch (e) {
+    console.log('Error conectando a la base de datos: ', e);
+    return res.status(500).json({ message: 'Error: ' + e });
+  }
 });
 
 app.get('/semestre/activo', async (req, res) => {
-   try {
-        const connection = mysql.createConnection(dbConfig);
-        connection.connect();
-        const query = `SELECT * FROM Periodo WHERE Estado = 'Activo';`;
-        const semestre = await runQuery(connection, query);
-        if (semestre.length == 0) {
-            return res.status(200).json({ message: 'No hay semestre activo' });
-        }
-        return res.status(200).json(semestre);
-    } catch (e) {
-        return res.status(500).json({ message: 'Error: '+e });
+  try {
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect();
+    const query = `SELECT * FROM Periodo WHERE Estado = 'Activo';`;
+    const semestre = await runQuery(connection, query);
+    if (semestre.length == 0) {
+      return res.status(200).json({ message: 'No hay semestre activo' });
     }
+    return res.status(200).json(semestre);
+  } catch (e) {
+    return res.status(500).json({ message: 'Error: ' + e });
+  }
 });
 
 app.listen(PORT, () => {
