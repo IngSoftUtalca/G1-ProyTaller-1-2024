de2edc921a38e660170a2067145bf0008a8597a1
rafa <rafajeremias@live.com>
2024-06-13 16:15:55 -0400

    Arreglo de Visual en el top de asistencia

--- mainpage/src/components/AsistenciaAdmin.vue
+++ mainpage/src/components/AsistenciaAdmin.vue
@@ -7,17 +7,23 @@
                         <div class="col container d-flex justify-content-start align-items-cente">
                             <button class="btn-primary-16 btn-size-120">
                             <span class="bold">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                                </svg>
                             </span>
                             </button>
                         </div>
                     <div class="col d-flex justify-content-center align-items-center">
-                        <p class="m-0 align-middle bold font-22">Semestre: {{ semestre }}</p>
+                        <p class="m-0 align-middle bold font-22">Semestre: 2024-1 {{ semestre }}</p>
                     </div>
             <div class="col d-flex justify-content-end align-items-center">
                 <div class="size-170-45 r-16 d-flex align-items-center justify-content-center">
                     <div class="col container d-flex justify-content-start align-items-cente">
                 <button class="btn-primary-16 btn-size-120">
                     <span class="bold">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                        </svg>
                     </span>
                 </button>
                 </div>
