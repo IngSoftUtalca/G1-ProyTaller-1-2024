a227e1a83e6ac83ad8c643212065a8b9b3d95922
CHenriquezG <cclaudiohenriquez@gmail.com>
2024-07-01 13:25:07 -0400

    cambios en la monitoreo
    se traslado a ms-gestionasistencia

---
+++ ms-gestorasistencia/verificaciones.js
@@ -0,0 +1,121 @@
+const mysql = require('mysql');
+process.env.TZ = 'America/Santiago'; 
+const dbData = require('../ENPOINTS.json').DB;
+const moment = require('moment-timezone');
+let semestreActual;
+
+
+function CalcularSemestre(){
+
+
+    const fechaActual = new Date();
+    const formatofecha = fechaActual.getFullYear()+"-"+fechaActual.getMonth()+"-"+fechaActual.getDay();
+    console.log(formatofecha);
+
+
+
+    const conection = mysql.createConnection(dbData);
+    conection.connect((error)=>{
+        if(error){
+            console.log(error);
+            return ;
+            
+        }
+        const query = `SELECT ID, FechaInicio, FechaTermino FROM Periodo ORDER BY FechaInicio DESC LIMIT 1;`
+        conection.query(query,(error,results)=>{
+            conection.end();
+            
+
+            if(error){
+                console.log("error.message");
+                return;
+            }else{
+                semestreActual = results[0].ID;
+                return results[0].ID;
+            }
+
+
+        });
+    });
+}
+
+
+function GetFechaHoy(){
+    const enChile = moment.tz("America/Santiago");
+    return enChile.format('YYYY-MM-DD')
+    return fechaActual.getFullYear()+"-"+(fechaActual.getMonth()+1)+"-"+fechaActual.getUTCDate();
+}
+
+
+function GetHoraActual(){
+    process.env.TZ = 'America/Santiago'; // se define la zona horaria en chile
+    const tiempoActual = new Date();
+    return tiempoActual.getHours()+":"+tiempoActual.getMinutes()+":"+tiempoActual.getSeconds();
+    
+}
+
+
+
+
+
+
+function CompararHoras(HoraA,MinutoA,SegundoA,HoraF,MinutoF,SegundoF)
+{
+    var actual = new Date();
+    var final = new Date();
+    actual.setHours(HoraA,MinutoA,SegundoA);
+
+    final.setHours(HoraF,MinutoF,SegundoF); 
+
+    if( actual <= final ){
+        return actual.getHours()+":"+actual.getMinutes()+":"+actual.getSeconds();
+    }else{
+        return final.getHours()+":"+final.getMinutes()+":"+final.getSeconds();
+    }
+}
+
+function CompararHorasBool(HoraA,MinutoA,SegundoA,HoraF,MinutoF,SegundoF)
+{
+    var actual = new Date();
+    var final = new Date();
+    actual.setHours(HoraA,MinutoA,SegundoA);
+
+    final.setHours(HoraF,MinutoF,SegundoF); 
+
+    if( actual < final ){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function CompararHorasFinDia(HoraA,MinutoA,SegundoA)
+{
+    var actual = new Date();
+    var inicio = new Date();
+    var final = new Date();
+
+    actual.setHours(HoraA,MinutoA,SegundoA);
+    inicio.setHours(8,30,0); 
+    final.setHours(22,20,0); 
+
+    if(actual >= inicio && actual < final ){
+        console.log('true')
+        return true;
+    }else{
+        console.log('false')
+        return false;
+    }
+}
+
+module.exports = {
+    CalcularSemestre,
+    GetFechaHoy,
+    GetHoraActual,
+    CompararHoras,
+    CompararHorasBool,
+    CompararHorasFinDia
+};
+
+
+
