501ae33fa99d3aa7104e99499baa4fb29ba83269
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-07-03 14:58:09 -0400

    Probando nueva logica con el sso

--- API-MAIN-ADMIN/inter.php
+++ API-MAIN-ADMIN/inter.php
@@ -4,6 +4,28 @@
     $id = isset($_GET['id']) ? $_GET['id'] : false;
     $v = isset($_GET['v']) ? $_GET['v'] : false;
     /* -------------------------------------- */
+
+    function loadEnv($path){
+        if(!file_exists($path)){
+            return false;
+        }
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach($lines as $line){
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+            list($name, $value) = explode('=', $line,2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+                $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
+        }
+    }
     
     $mainpage = file_get_contents('../ENPOINTS.json');
     $mainpage = json_decode($mainpage, true);
@@ -18,6 +40,8 @@
         /* ----------------------------------------------------------- */
     } elseif ($v) {
 
+        loadEnv(__DIR__.'/.env');
+
         /* Configuración de sesión - Según lo requerido en el sistema, tener en cuenta que sesión del IDP-SP dura 25 min*/
         error_reporting(-1);
         ini_set('display_errors', 'On');
@@ -26,6 +50,20 @@
         session_start();
         $_SESSION['id'] = $id;
         $_SESSION['sso'] = true;
+
+        $cipher = "aes-128-gcm";
+
+        $options = 0;
+
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+
+        $FLAG = getenv('FLAG');
+        $KEY = getenv('KEY');
+
+        $encryptedFlag = openssl_encrypt($FLAG, $cipher, $KEY, $options, $iv);
+
+        $mainpage .= "?flag=".$encryptedFlag."&iv=".base64_encode($iv);
+
 
         /*-------------------------*/ 
         /* Link de página de inicio sistema a loguear - Modificable*/
